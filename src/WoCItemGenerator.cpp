7c6db1a80eb81ae1ff97bbc04571567340f6507d
AnchyDev <lewienato@gmail.com>
2022-11-19 11:25:49 +1100

    Added basic dynamic virtual item generation.

--- src/WoCItemGenerator.cpp
+++ src/WoCItemGenerator.cpp
@@ -1,7 +1,25 @@
 #include "WoCItemGenerator.h"
 
-void WoCPlayer::OnStoreNewItem(Player* player, Item* /*item*/, uint32 /*count*/)
+bool WoCPlayer::IsWoCItem(const ItemTemplate* itemTemplate)
 {
+    return (itemTemplate->FlagsCu & WOC_FLAGS_ITEM) == WOC_FLAGS_ITEM;
+}
+
+void WoCPlayer::OnStoreNewItem(Player* player, Item* item, uint32 /*count*/)
+{
+    auto itemTemplate = item->GetTemplate();
+
+    if (!IsWoCItem(itemTemplate))
+    {
+        return;
+    }
+
+    WoCItem* wocItem = new WoCItem(itemTemplate);
+    wocItem->ItemId = currentId;
+    wocItem->Name1 = Acore::StringFormat("{} +{}", wocItem->Name1, currentId);
+
+    currentId += 1;
+
 	ChatHandler(player->GetSession()).SendSysMessage("Stored New Item.");
 }
 
--- src/WoCItemGenerator.h
+++ src/WoCItemGenerator.h
@@ -6,13 +6,29 @@
 #include "Config.h"
 #include "Chat.h"
 
+class WoCItem : public ItemTemplate
+{
+public:
+    WoCItem(ItemTemplate const* base) : ItemTemplate(*base) { }
+};
+
 class WoCPlayer : public PlayerScript
 {
 public:
-    WoCPlayer() : PlayerScript("WoCPlayer") { }
+    WoCPlayer() : PlayerScript("WoCPlayer")
+    {
+        currentId = 410001;
+    }
 
 private:
-    void OnStoreNewItem(Player* player, Item* /*item*/, uint32 /*count*/) override;
+    uint32 currentId;
+
+    enum WoCFlags
+    {
+        WOC_FLAGS_ITEM = 16
+    };
+    bool IsWoCItem(const ItemTemplate* /*itemTemplate*/);
+    void OnStoreNewItem(Player* /*player*/, Item* /*item*/, uint32 /*count*/) override;
 };
 
 #endif
