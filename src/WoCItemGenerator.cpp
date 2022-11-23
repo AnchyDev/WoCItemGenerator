ebfe51decee0ae00f034f82a0d84b17ce1ffeb42
AnchyDev <lewienato@gmail.com>
2022-11-23 12:32:40 +1100

    Reimplemented suffix change function.

--- src/WoCItemGenerator.cpp
+++ src/WoCItemGenerator.cpp
@@ -5,6 +5,33 @@ bool WoCPlayer::IsWoCItem(const ItemTemplate* itemTemplate)
     return (itemTemplate->FlagsCu & WOC_FLAGS_ITEM) == WOC_FLAGS_ITEM;
 }
 
+void SetSuffix(Item* item, int32 randomPropId)
+{
+    ItemRandomSuffixEntry const* item_rand = sItemRandomSuffixStore.LookupEntry(randomPropId);
+
+    if (!item_rand)
+    {
+        LOG_INFO("module", "Could not find Random Suffix: {} from sItemRandomSuffixStore.", randomPropId);
+        return;
+    }
+
+    if (item->GetInt32Value(ITEM_FIELD_RANDOM_PROPERTIES_ID) != -int32(item_rand->ID) ||
+        !item->GetItemSuffixFactor())
+    {
+        item->SetInt32Value(ITEM_FIELD_RANDOM_PROPERTIES_ID, -int32(item_rand->ID));
+        item->UpdateItemSuffixFactor();
+        item->SetState(ITEM_CHANGED, item->GetOwner());
+    }
+    else
+    {
+        LOG_INFO("module", "Failed ITEM_FIELD_RANDOM_PROPERTIES_ID & GetItemSuffixFactor check.");
+        return;
+    }
+
+    for (uint32 i = PROP_ENCHANTMENT_SLOT_0; i < MAX_ENCHANTMENT_SLOT; ++i)
+        item->SetEnchantment(EnchantmentSlot(i), item_rand->Enchantment[i - PROP_ENCHANTMENT_SLOT_0], 0, 0);
+}
+
 void WoCPlayer::OnStoreNewItem(Player* player, Item* item, uint32 /*count*/)
 {
     auto itemTemplate = item->GetTemplate();
@@ -18,7 +45,8 @@ void WoCPlayer::OnStoreNewItem(Player* player, Item* item, uint32 /*count*/)
     ChatHandler(player->GetSession()).SendSysMessage(Acore::StringFormat("Rolled {}", roll));
     if (roll > 50.0)
     {
-        item->SetItemRandomProperties(-441101);
+        SetSuffix(item, 441101);
+        //item->SetItemRandomProperties(-441101);
         ChatHandler(player->GetSession()).SendSysMessage("Won roll");
     }
 
