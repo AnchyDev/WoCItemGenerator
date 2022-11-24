9ddccca71d1f40639b225a199b008caf3b82f31b
AnchyDev <lewienato@gmail.com>
2022-11-24 20:44:37 +1100

    Use mutable template to apply socket.

--- src/WoCItemGenerator.cpp
+++ src/WoCItemGenerator.cpp
@@ -175,6 +175,8 @@ void WoCPlayer::OnStoreNewItem(Player* player, Item* item, uint32 /*count*/)
         SetSuffix(item, 100);
         ChatHandler(player->GetSession()).SendSysMessage("Won roll");
 
+        item->GetMutableTemplate()->Socket[0].Color = SOCKET_COLOR_RED;
+
         LOG_INFO("module", "RandomSuffixId {}", item->GetItemRandomPropertyId());
     }
 }
